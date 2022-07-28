6b67ff10a1f762cee48a60209032cae3d43e6500
Piglet <piglet20181112@outlook.com>
2022-07-28 08:35:32 -0700

    customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,56 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+      },
+    },
+    fontFamily: {
+      'lato': ['Lato', 'Arial'],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '0'
+        // lg: '4rem',
+        // xl: '5rem',
+        // '2xl': '6rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "600px",
+          },
+          "@screen md": {
+            maxWidth: "100%",
+          },
+          "@screen lg": {
+            maxWidth: "100%",
+          },
+          "@screen xl": {
+            maxWidth: "100%",
+          },
+          "@screen 2xl": {
+            maxWidth: "100%",
+          },
+          "@screen 3xl": {
+            maxWidth: "1500px",
+          },
+        },
+      });
+    },
+  ],
 }
